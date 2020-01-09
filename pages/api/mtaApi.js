177cc85e94575fbff10f6b29f25047dedba4ce25
Will Strasser <wstrasser@hioscar.com>
2020-01-08 22:41:16 -0500

    tinker

--- pages/api/mtaApi.js
+++ pages/api/mtaApi.js
@@ -1,5 +1,7 @@
 import Mta from 'mta-gtfs';
 
+console.log('\n\n\n\n\nMTA_API_KEY', process.env.MTA_API_KEY);
+
 export const mtaApi = new Mta({
   key: process.env.MTA_API_KEY,
   feed_id: 1,
--- pages/api/testing.js
+++ pages/api/testing.js
@@ -1,12 +1,7 @@
 import {mtaApi} from './mtaApi';
 
 export default (req, res) => {
-  mtaApi
-    .schedule(122)
-    .then(function(result) {
-      res.json(result.schedule['122'].N);
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+  mtaApi.schedule(122).then(function(result) {
+    res.json(result.schedule['122'].N);
+  });
 };
